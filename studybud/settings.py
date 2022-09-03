2f00e243155d131b663c0274123a3d4b4142d8b6
Akobir <akobirnarzullayev41@gmail.com>
2022-09-03 13:17:39 +0500

    final commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p96&79@+m8*r=g5p-(fl&-b)%)rr5s$6op_k&gt!74&)9!k1gw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['study-buddy-uz.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
