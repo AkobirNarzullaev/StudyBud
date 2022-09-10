84fe44b70e8f1aafc17581ab1b730fb6d4395543
Akobir <akobirnarzullayev41@gmail.com>
2022-09-10 11:52:08 +0500

    changed DEBUG to True

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p96&79@+m8*r=g5p-(fl&-b)%)rr5s$6op_k&gt!74&)9!k1gw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
